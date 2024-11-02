267e84f0b670153f50b3b998485dad33d00f132b
juh4212 <91859121+juh4212@users.noreply.github.com>
2024-11-03 04:12:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,12 @@
 
 import logging
 import threading
-from discord_bot import run_discord_bot_thread  # 수정된 임포트
+from discord_bot import run_discord_bot_thread
 from scheduler import scheduler_job
-
-def start_scheduler():
-    scheduler_job()
-
-def start_discord_bot():
-    run_discord_bot_thread()  # 코루틴을 올바르게 실행
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.INFO,  # 필요 시 DEBUG로 변경
         format='%(asctime)s:%(levelname)s:%(message)s',
         handlers=[
             logging.StreamHandler()
@@ -21,9 +15,9 @@ if __name__ == "__main__":
     )
     
     # 스케줄러를 별도의 스레드에서 실행
-    scheduler_thread = threading.Thread(target=start_scheduler, daemon=True)
+    scheduler_thread = threading.Thread(target=scheduler_job, daemon=True)
     scheduler_thread.start()
     logging.info("스케줄러가 별도의 스레드에서 시작되었습니다.")
     
     # Discord 봇 실행
-    start_discord_bot()
+    run_discord_bot_thread()
